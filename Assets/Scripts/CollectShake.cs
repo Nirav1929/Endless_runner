4eb8f4a9174167ce65e9e99014415e3cd5d943c5
ptaneja <ptaneja@ncsu.edu>
2022-03-28 23:36:28 -0400

    Fixing File find bug

--- Assets/Scripts/CollectShake.cs
+++ Assets/Scripts/CollectShake.cs
@@ -6,9 +6,12 @@ public class CollectShake : MonoBehaviour
 {
     void OnTriggerEnter (Collider other){
         // cameraScript = GameObject.Find("MainCamera").GetComponent(CameraShake);
-        cameraScript = GameObject.FindObjectOfType(typeof(CameraShake)) as CameraShake;
-        Debug.Log("I'm colliding with: " + cameraScript);
-        cameraScript.shakeCamera();
+        GameObject go = GameObject.Find("MainCamera");
+        CameraShake ot = (CameraShake) go.GetComponent(typeof(CameraShake));
+
+        // cameraScript = GameObject.FindObjectOfType(typeof(CameraShake)) as CameraShake;
+        // Debug.Log("I'm colliding with: " + cameraScript);
+        ot.shakeCamera();
         this.gameObject.SetActive(false);
     }
 }
